42f52b7b9675bae48e09689e4af5ce14c3449dae
Rishi Sayanthakumar <160595074+Christumas@users.noreply.github.com>
2025-03-16 15:02:12 +0100

    changed hamburger menu to arrow, added functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () =>{
+    const toggleButton = document.querySelector(".toggle-button");
+    const navLinks = document.querySelector(".nav-links");
+    const arrow = document.querySelector(".arrow");
+
+    toggleButton.addEventListener("click", () =>{
+        if (navLinks.style.display === "flex") {
+            // If menu is already visible, hide it and reset arrow
+            navLinks.style.display = "none";
+            arrow.style.transform = "rotate(0deg)";
+        } else {
+            // If menu is hidden, show it and rotate arrow
+            navLinks.style.display = "flex";
+            arrow.style.transform = "rotate(180deg)";
+        }
+    })
+})
